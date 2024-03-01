4fd6ab1f4cb680d1925d4b7d92a71ba829c156f0
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2024-03-01 15:28:57 +0530

    my second commit

--- 5_NPM/index_npmtest.js
+++ 5_NPM/index_npmtest.js
@@ -1,6 +1,6 @@
 const figlet = require('figlet');
 
-figlet("NIKKY I love You My Cutie Pie!!", function (err, data) {
+figlet("Smit is an excellent coder!!", function (err, data) {
     if (err) {
       console.log("Something went wrong...");
       console.dir(err);
